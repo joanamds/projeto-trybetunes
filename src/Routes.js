ca49d635d3873c4ce941b42c39e5660a96759a55
Joana <joanamdsantos1@gmail.com>
2022-10-03 15:40:36 -0300

    criando os componentes e as rotas, primeiro requisito finalizado

---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import NotFound from './components/NotFound';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
---
+++ src/components/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <h1>ProfileEdit</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
