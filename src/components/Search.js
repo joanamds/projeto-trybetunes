df85266d9716c3209afc59905e31c9dea17d3a53
Joana <joanamdsantos1@gmail.com>
2022-10-04 11:56:28 -0300

    finalizando o quinto requisito

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,49 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isDisabled: true,
+      searchItem: '',
+    };
+  }
+
+  onSearchChange = (event) => {
+    const searchValue = event.target.value;
+    this.setState({
+      searchItem: searchValue,
+    }, () => this.validateSearch());
+  };
+
+  validateSearch = () => {
+    const { searchItem } = this.state;
+    const minLength = 2;
+    const validSearch = searchItem.length >= minLength;
+    this.setState({
+      isDisabled: !validSearch,
+    });
+  };
+
   render() {
+    const { isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header data-testid="header-component" />
-        <h1>Search</h1>
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          placeholder="Nome do Artista"
+          onChange={ this.onSearchChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ isDisabled }
+        >
+          Procurar
+        </button>
       </div>
     );
   }
